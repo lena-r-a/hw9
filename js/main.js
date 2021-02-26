75483f3e33804eb47047ad44d01905120d578ce9
lena-r-a <75614947+lena-r-a@users.noreply.github.com>
2021-02-26 12:46:27 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,84 @@
+$(function () {
+  var index = 0;
+  const addButton = $("#save");
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = dd + '/' + mm + '/' + yyyy;
+console.log(today);
+
+  if (localStorage.getItem("number")) {
+    index = localStorage.getItem("number");
+  }
+
+  if (localStorage.getItem("tasks")) {
+    $("ul").append(localStorage.getItem("tasks"));
+  } 
+
+  $('#newtask').on('shown.bs.modal', function () {
+    //#myInput - id элемента, которому необходимо установить фокус
+    $('#task__name').focus();
+  })
+
+  addButton.click(function () {
+    let date = document.querySelector("#finish__date");
+    let tn = $("#task__name").val();
+    if (tn !== "") {
+      index++;
+      $(".list__task")
+        .append(`<li class="row"><div class="left-cont col-6"><input type="checkbox" 
+             id="check-${index}" name=""><label for="check-'${index}'"></label><span>${tn}</span></div>
+             <span class="col date">${date.value}</span>
+              <span class="remove col"><i class="far fa-trash-alt"></i></span></li>`);
+    }
+    if (date.value!==""){$('.date').attr('data-tooltip','Срок выполнения задачи')};
+    $("#finish__date").val("YYYY-MM-DD");
+    $("#task__name").val("");
+    toLocal();
+  });
+
+  $(document).on("click", ".remove", function () {
+    $(this).parent().remove();
+    console.log($(this).parent());
+    console.log(index);
+    toLocal();
+  });
+
+  function toLocal() {
+    let list = $("ul").html();
+    localStorage.setItem("tasks", list);
+    localStorage.setItem("number", index);
+  }
+  $(document).on("change", 'input[type="checkbox"]', function () {
+    if ($(this).is(":checked")) {
+      $(this).siblings("span").addClass("strikethrow");
+      $(this).attr("checked", true);
+      toLocal();
+    } else {
+      $(this).siblings("span").removeClass("strikethrow");
+      $(this).attr("checked", false);
+      toLocal();
+    }
+  });
+
+  let list = document.querySelector("ul");
+  list.addEventListener('click',function(e){
+    debugger;
+    let current = e.target;
+     if (current.tagName === "SPAN") {(current.classList.toggle("strikethrow"));
+                                      current.closest('div').firstChild.toggleAttribute("checked")};
+    toLocal();
+  });
+
+
+  $("#clear").click(() => {
+    localStorage.clear();
+    $("ul li").remove();
+    index = 0;
+  });
+});
+
+
